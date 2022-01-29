b45650db6d955c3aed42ea76450b9110ab8237d5
AlexandrLo <alexandr.lo@outlook.com>
2022-01-29 23:55:23 +0300

    Delete unused js

--- index.js
+++
@@ -1,9 +0,0 @@
-function changeCss() {
-	var navElement = document.querySelector("nav");
-	if (this.scrollY > 0) {
-		navElement.classList.add("navbar-scrolled");
-	} else {
-		navElement.classList.remove("navbar-scrolled");
-	}
-}
-window.addEventListener("scroll", changeCss, false);
