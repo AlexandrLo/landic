0aa53ac2da21ab991487e504e212c1557386ac7a
AlexandrLo <alexandr.lo@outlook.com>
2022-03-21 19:25:07 +0300

    Fix transition on page load

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,7 @@
 import "../scss/style.scss";
 import "phosphor-icons";
+
+document.addEventListener("DOMContentLoaded", function () {
+  let node = document.querySelector(".preload");
+  node.classList.remove("preload");
+});
